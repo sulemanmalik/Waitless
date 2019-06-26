5bb6d212526a12c0c968ce30e49d243a94fa0f7e
sulemanmalik <sulemanmalik@Sulemans-MBP.lan>
2019-06-25 22:58:22 -0400

    chore: route from signup to login and vice versa

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -251,7 +251,7 @@ const Signup = () => {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/login" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
